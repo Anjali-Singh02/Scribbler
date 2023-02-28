2566a8c9d4a8c403af20ba84093103029570cacf
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-03-01 00:36:27 +0530

    update file

--- script/index.js
+++ script/index.js
@@ -38,3 +38,23 @@ closeIcon1.onclick = function() {
 }
 
 
+//  ******* CREATE POST MODAL ******************
+const createmodal = document.getElementById("createModal");
+
+// Get the button that opens the modal
+const createBtn = document.getElementById("createPost");
+
+// Get the <span> element that closes the modal
+const closeIcon2 = document.getElementById("closeCreate");
+
+// When the user clicks the button, open the modal 
+createBtn.onclick = function() {
+  createmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeIcon2.onclick = function() {
+  createmodal.style.display = "none";
+}
+
+
