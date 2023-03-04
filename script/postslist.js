812a5c9cc061163280bcdfed72d7473fd50e8c4b
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-03-05 02:56:56 +0530

    updated edit

--- script/postslist.js
+++ script/postslist.js
@@ -2,7 +2,19 @@
 
 const deleteIcon = document.getElementsByClassName("deleteIcon")
 const deleteModal = document.getElementById("deleteBlog")
+const yesBtn = document.getElementById("yesBtn")
+const noBtn = document.getElementById("noBtn")
+const deletePost =() => {
+    deleteModal.style.display = "block"
+}
 
-deleteIcon.addEventListener("click", (event) => { 
-deleteModal.style.display = block;
-})
+// yesBtn.addEventListener = ('click',(event) => {
+//     deleteModal.style.display = "none"
+// })
+
+const yes = () => {
+    deleteModal.style.display = "none"
+}
+const no = () => {
+    deleteModal.style.display = "none"
+}
