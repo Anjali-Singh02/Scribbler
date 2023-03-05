7561799d61bad48691d7960e6136d29c85bbe8c5
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-03-05 21:20:39 +0530

    updated delete button

--- script/postslist.js
+++ script/postslist.js
@@ -4,17 +4,38 @@ const deleteIcon = document.getElementsByClassName("deleteIcon")
 const deleteModal = document.getElementById("deleteBlog")
 const yesBtn = document.getElementById("yesBtn")
 const noBtn = document.getElementById("noBtn")
-const deletePost =() => {
+const deletePost =(post1) => {
     deleteModal.style.display = "block"
+    console.log(post1)
 }
 
 // yesBtn.addEventListener = ('click',(event) => {
 //     deleteModal.style.display = "none"
 // })
 
-const yes = () => {
-    deleteModal.style.display = "none"
-}
 const no = () => {
     deleteModal.style.display = "none"
+}
+
+
+
+
+//  Post List function
+
+var id="";
+function setId(htmlId){
+    deleteModal.style.display="block";
+    id=htmlId;
+}
+function yes(){
+    deleteModal.style.display="none";
+    document.getElementById(id).style.display="none";
+}
+function settingContentOfId(getUser,getTitle,getContent){
+    let userName=document.getElementById(getUser).innerText;
+    localStorage.setItem('userName',userName);
+    let titleOfPost=document.getElementById(getTitle).innerText;
+    localStorage.setItem('title',titleOfPost);
+    let contentOfPost=document.getElementById(getContent).innerText;
+    localStorage.setItem('content',contentOfPost);
 }
